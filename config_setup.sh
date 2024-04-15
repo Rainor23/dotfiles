1ad6381f6f7095f1be9646ee4684bd3c71348b18
Rainerd0x21 <45594693+Rainor23@users.noreply.github.com>
2024-04-15 13:55:41 +0200

    Update config_setup.sh

--- config_setup.sh
+++ config_setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+read -p "Username of installer: " username
+
 echo "Making sure packages are up to date"
 sudo apt update; sudo apt upgrade -y
 
-config_dir="$HOME/.config"
+config_dir="/home/$username/.config"
 
 echo "checking if $HOME/.config exists..."
 if [ -d "$config_dir" ]; then
@@ -18,58 +20,73 @@ fi
 echo -e "\n- Installing i3"
 echo "##Installing required packages##"
 if ! dpkg -s i3 >/dev/null 2>&1; then
-    sudo apt install i3 -y
+    sudo apt install i3 -y >/dev/null 2>&1
 else
     echo "i3 is already installed."
 fi
 
+echo -e "\n- Installing picom"
+if ! dpkg -s polybar >/dev/null 2>&1; then
+    sudo apt install picom -y >/dev/null 2>&1
+else
+    echo "polybar is already installed."
+fi
+
 echo -e "\n- Installing polybar"
 if ! dpkg -s polybar >/dev/null 2>&1; then
-    sudo apt install polybar -y
+    sudo apt install polybar -y >/dev/null 2>&1
 else
     echo "polybar is already installed."
 fi
 
 echo -e "\n- Installing rofi"
 if ! dpkg -s rofi >/dev/null 2>&1; then
-    sudo apt install rofi -y
+    sudo apt install rofi -y >/dev/null 2>&1
 else
     echo "rofi is already installed."
 fi
 
+echo -e "\n- Installing alacritty"
+if ! dpkg -s alacritty >/dev/null 2>&1; then
+    sudo apt install alacritty -y >/dev/null 2>&1
+else
+    echo "alacritty is already installed."
+fi
+
 echo -e "\n- Installing feh"
 if ! dpkg -s feh >/dev/null 2>&1; then
-    sudo apt install feh -y
+    sudo apt install feh -y >/dev/null 2>&1
 else
     echo "feh is already installed."
 fi
 
 echo -e "\n- Installing zsh"
 if ! dpkg -s zsh >/dev/null 2>&1; then
-    sudo apt install zsh -y
+    sudo apt install zsh -y >/dev/null 2>&1
 else
     echo "zsh is already installed."
 fi
 
 echo -e "\n- Installing Oh-My-ZSH"
-if [ -d "$HOME/.oh-my-zsh" ]; then
+if [ -d "/home/$username/.oh-my-zsh" ]; then
     echo "Oh My Zsh is already installed."
 else
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" >/dev/null 2>&1
 fi
 
+exit
 echo -e "\n- Installing zsh-autosuggestions"
 if [ -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
     echo zsh-autosuggestions is already installed
 else
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions >/dev/null 2>&1
 fi
 
 echo -e "\n- Installing zsh-syntax-highlighting"
 if [ -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
     echo zsh-syntax-highlighting is already installed
 else
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting >/dev/null 2>&1
 fi
 
 echo -e "\nAdding config files"
@@ -99,8 +116,8 @@ cp ./config $config_dir/i3/
 cp ./picom.conf $config_dir/picom/
 cp ./config.rasi $config_dir/rofi/
 cp ./config.ini $config_dir/polybar/
-cp ./startup.sh $config_dir/
-cp ./.zshrc $HOME
+cp ./startup.sh $config_dir/; chmod +x $config_dir/startup.sh
+cp ./.zshrc /home/$username/
 
 echo -e "\nCreating xsession desktop"
 sudo cp ./i3.desktop /usr/share/xsessions/
